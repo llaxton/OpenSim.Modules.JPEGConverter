8410e4a60095583dcedabaed3606cf67a682b53e
Unknown <christopher.latza@gmail.com>
2017-09-11 15:17:24 +0200

    add

---
+++ src/JPEGConverter.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Addins;
+using OpenSim.Region.Framework.Interfaces;
+using log4net;
+using System.Reflection;
+using OpenSim.Region.Framework.Scenes;
+using OpenSim.Services.Interfaces;
+using OpenSim.Framework.Servers.HttpServer;
+using OpenSim.Framework.Servers;
+using Nini.Config;
+
+[assembly: Addin("JPEGConverter", "0.2")]
+[assembly: AddinDependency("OpenSim.Region.Framework", OpenSim.VersionInfo.VersionNumber)]
+
+namespace OpenSim.Modules.JPEGConverter
+{
+    [Extension(Path = "/OpenSim/RegionModules", NodeName = "RegionModule")]
+    class JPEGConverter : ISharedRegionModule
+    {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private IAssetService m_assetService;
+        private IConfigSource m_config;
+
+        public IAssetService AssetService
+        {
+            get
+            {
+                return m_assetService;
+            }
+        }
+
+        public ILog Log
+        {
+            get
+            {
+                return m_log;
+            }
+        }
+
+        public void RegionLoaded(Scene scene)
+        {
+            if (scene != null)
+            {
+                m_assetService = scene.AssetService;
+                m_log.Info("[JPEGConverter] Load into region " + scene.Name);
+            }
+
+            IHttpServer server = MainServer.GetHttpServer(0);
+            server.AddStreamHandler(new JPEGConverterHandler(this));
+        }
+
+        public void PostInitialise()
+        {
+            m_log.Info("[JPEGConverter] PostInitialise");
+        }
+
+        public string Name
+        { 
+            get
+            {
+                return "JPEGConverter"; 
+            }
+        }
+
+        public Type ReplaceableInterface 
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public void Initialise(IConfigSource source)
+        {
+            m_config = source;
+            m_log.Info("[JPEGConverter] Initialise");
+        }
+
+        public void Close()
+        {
+
+        }
+
+        public void AddRegion(Scene scene)
+        {
+
+        }
+
+        public void RemoveRegion(Scene scene)
+        {
+
+        }
+    }
+}
---
+++ src/JPEGConverterHandler.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenSim.Framework.Servers.HttpServer;
+using System.IO;
+using OpenSim.Framework;
+using OpenMetaverse;
+using System.Drawing;
+using OpenSim.Services.Interfaces;
+using Nini.Config;
+using OpenSim.Region.CoreModules.ServiceConnectorsOut.Asset;
+using log4net;
+using System.Reflection;
+
+namespace OpenSim.Modules.JPEGConverter
+{
+    class JPEGConverterHandler : BaseStreamHandler
+    {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private JPEGConverter m_JPEGConverter = null;
+
+        public JPEGConverterHandler(JPEGConverter jpeg) : base("GET", "/jpeg/")
+        {
+            if (m_JPEGConverter == null)
+            {
+                m_JPEGConverter = jpeg;
+            }
+        }
+
+        static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        private static byte[] ImageToByte(Image img)
+        {
+            ImageConverter converter = new ImageConverter();
+            return (byte[])converter.ConvertTo(img, typeof(byte[]));
+        }
+
+        public static byte[] ReadToEnd(System.IO.Stream stream)
+        {
+            long originalPosition = 0;
+
+            if (stream.CanSeek)
+            {
+                originalPosition = stream.Position;
+                stream.Position = 0;
+            }
+
+            try
+            {
+                byte[] readBuffer = new byte[4096];
+
+                int totalBytesRead = 0;
+                int bytesRead;
+
+                while ((bytesRead = stream.Read(readBuffer, totalBytesRead, readBuffer.Length - totalBytesRead)) > 0)
+                {
+                    totalBytesRead += bytesRead;
+
+                    if (totalBytesRead == readBuffer.Length)
+                    {
+                        int nextByte = stream.ReadByte();
+                        if (nextByte != -1)
+                        {
+                            byte[] temp = new byte[readBuffer.Length * 2];
+                            Buffer.BlockCopy(readBuffer, 0, temp, 0, readBuffer.Length);
+                            Buffer.SetByte(temp, totalBytesRead, (byte)nextByte);
+                            readBuffer = temp;
+                            totalBytesRead++;
+                        }
+                    }
+                }
+
+                byte[] buffer = readBuffer;
+                if (readBuffer.Length != totalBytesRead)
+                {
+                    buffer = new byte[totalBytesRead];
+                    Buffer.BlockCopy(readBuffer, 0, buffer, 0, totalBytesRead);
+                }
+                return buffer;
+            }
+            finally
+            {
+                if (stream.CanSeek)
+                {
+                    stream.Position = originalPosition;
+                }
+            }
+        }
+
+        protected override byte[] ProcessRequest(string path, Stream requestData, IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
+        {
+            Dictionary<string, object> request = new Dictionary<string, object>();
+            foreach (string name in httpRequest.QueryString)
+                request[name] = httpRequest.QueryString[name];
+
+            IAssetService _assetService = m_JPEGConverter.AssetService;
+            
+            if (request.ContainsKey("assetURL"))
+            {
+                string _assetServerURL = Convert.ToString(request["assetURL"]);
+
+                IConfigSource _config = new IniConfigSource();
+                _config.AddConfig("Modules");
+                _config.AddConfig("AssetService");
+                _config.Configs["AssetService"].Set("AssetServerURI", _assetServerURL);
+                _config.Configs["Modules"].Set("AssetServices", @"RemoteAssetServicesConnector");
+
+                RemoteAssetServicesConnector _rsc = new RemoteAssetServicesConnector();
+                _rsc.Initialise(_config);
+                _assetService = _rsc;
+            }
+
+            if (_assetService != null)
+            {
+                string assetUUID = Convert.ToString(request["assetID"]);
+
+                if (_assetService.AssetsExist(new String[] {assetUUID})[0])
+                {
+                    AssetBase asset = _assetService.Get(assetUUID);
+
+                    if (asset != null)
+                    {
+                        if (asset.Type == (sbyte)AssetType.Texture)
+                        {
+                            OpenMetaverse.Imaging.ManagedImage jpegImageData = new OpenMetaverse.Imaging.ManagedImage(256, 256, OpenMetaverse.Imaging.ManagedImage.ImageChannels.Color);
+
+                            if (OpenMetaverse.Imaging.OpenJPEG.DecodeToImage(asset.Data, out jpegImageData))
+                            {
+                                Stream imageStream = new MemoryStream(ImageToByte(jpegImageData.ExportBitmap()));
+                                Stream saveStream = new MemoryStream();
+
+                                System.Drawing.Image image = System.Drawing.Image.FromStream(imageStream);
+                                image.Save(saveStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                                httpResponse.ContentType = "image/jpeg";
+                                return ReadToEnd(saveStream);
+                            }else
+                            {
+                                m_log.Info("[JPEGConverter] ERROR AT DECODE IMAGE DATA");
+                            }
+                        }else
+                        {
+                            m_log.Info("[JPEGConverter] ERROR: ASSET IS NOT AN IMAGE");
+                        }
+                    }else
+                    {
+                        m_log.Info("[JPEGConverter] ERROR: ASSET IS NULL");
+                    }
+                }
+            }else
+            {
+                m_log.Info("[JPEGConverter] ERROR: NO ASSET SERVICE");
+            }
+
+            return null;
+        }
+    }
+}
